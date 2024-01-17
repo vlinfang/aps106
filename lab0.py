67db389de09434cb14d513c130ca31bdea276059
vlinfang <156881728+vlinfang@users.noreply.github.com>
2024-01-17 01:03:30 -0500

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,9 @@
+###################################
+# APS106 Winter 2024 Lab 0
+###################################
+
+## Print a welcome message to the user
+name = "Valentina Lin Fang" # TODO: Enter your name here
+welcome_msg = "Hi " + name + ", Welcome to APS106!"
+print(welcome_msg)
+
